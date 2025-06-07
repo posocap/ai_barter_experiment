558eebd422c9a2f95d5d9d63c5b0522cb194610d
postsocialist <info@posocap>
2025-06-06 20:36:55 -0400

    rm deprecated test results RDS

--- main.R
+++ main.R
@@ -120,7 +120,6 @@ log_messages$sender <- gsub(log_messages$sender, pattern = "thread_",
                             replacement = "Agent ")
 
 timestamp <- format(Sys.time(), "%Y%m%d_%H%M%S")
-saveRDS(test_results, paste0("test_results_", timestamp, "_", ai_model, ".rds"))
 saveRDS(log_messages, paste0("log_messages_information_sharing_", 
                              timestamp, "_", ai_model, ".rds"))
 
